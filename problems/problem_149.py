f12c27a33657b13979efe784fc031bc491e0d6d6
hwmaltby <hwmaltby@berkeley.edu>
2017-12-13 23:57:06 -0800

    working on problem 148

---
+++ problems/problem_149.py
@@ -0,0 +1,14 @@
+#Henry Maltby
+import time
+import math
+
+def solve(k, n):
+    """
+    .
+    """
+    return n / 2
+
+start = time.time()
+K, N = 7, 1000000000
+print(solve(M, N))
+print("Elapsed time: {:0.4f}".format(time.time() - start))
